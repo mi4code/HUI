9802f64a1f30c96b6e5d35630a12e2a04f46ed1c
mi4code <114027049+mi4code@users.noreply.github.com>
2025-04-15 23:26:50 +0200

    no print

--- build_python.py
+++ build_python.py
@@ -27,7 +27,7 @@ if system == "Windows":
         pyname = "python"+str(sys.version_info.major)+str(sys.version_info.minor)  # python312
         suffix = sysconfig.get_config_var('EXT_SUFFIX')  # .cp312-win_amd64.pyd
         #os.system("call \"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat\"  &&  cl /LD /EHsc /O2 /Fe:HUI"+suffix+" ../hui_python.cc "+includes+" /I\"..\" /link /LIBPATH:\""+libpath+"\\libs"+"\" "+pyname+".lib libHUI.lib")
-        print("cl /LD /EHsc /O2 /Fe:HUI"+suffix+" ../hui_python.cc "+includes+" /I\"..\" /link /LIBPATH:\""+libpath+"\\libs"+"\" "+pyname+".lib libHUI.lib")
+        #print("cl /LD /EHsc /O2 /Fe:HUI"+suffix+" ../hui_python.cc "+includes+" /I\"..\" /link /LIBPATH:\""+libpath+"\\libs"+"\" "+pyname+".lib libHUI.lib")
         os.system("cl /LD /EHsc /O2 /Fe:HUI"+suffix+" ../hui_python.cc "+includes+" /I\"..\" /link /LIBPATH:\""+libpath+"\\libs"+"\" "+pyname+".lib libHUI.lib")
         
     print("done")

040733a9fd75b356d359d76cf4b78b58e5c3bdea
mi4code <114027049+mi4code@users.noreply.github.com>
2025-04-15 22:22:18 +0200

    add WindowControls

--- hui_python.cc
+++ hui_python.cc
@@ -42,7 +42,64 @@ PYBIND11_MODULE(HUI, m) {
 		.def("exit", &WebView::exit)
 		
 		;
-		
-	// TODO: WindowControls
 
+	
+	py::class_<WindowControls>(m, "WindowControls")
+		.def(py::init<void*, void*>(), py::arg("backend"), py::arg("handle"))
+		
+		.def("set_type", &WindowControls::set_type, py::arg("type"))
+		.def("get_type", &WindowControls::get_type)
+		
+		.def("set_layer", &WindowControls::set_layer, py::arg("layer"))
+		.def("get_layer", &WindowControls::get_layer)
+		
+		.def("set_geometry", &WindowControls::set_geometry, py::arg("geometry"))
+		.def("get_geometry", &WindowControls::get_geometry)
+		
+		.def("set_opacity", &WindowControls::set_opacity, py::arg("opacity"))
+		.def("get_opacity", &WindowControls::get_opacity)
+		
+		.def("set_id", &WindowControls::set_id, py::arg("id"))
+		.def("get_id", &WindowControls::get_id)
+		
+		.def("set_title", &WindowControls::set_title, py::arg("title"))
+		.def("get_title", &WindowControls::get_title)
+		
+		.def("set_exclusive_zone", &WindowControls::set_exclusive_zone, py::arg("zone"))
+		.def("get_exclusive_zone", &WindowControls::get_exclusive_zone)
+		
+		.def("set_focused", &WindowControls::set_focused, py::arg("activated"))
+		.def("get_focused", &WindowControls::get_focused)
+		
+		.def("set_input_mode_keyboard", &WindowControls::set_input_mode_keyboard, py::arg("mode"))
+		.def("get_input_mode_keyboard", &WindowControls::get_input_mode_keyboard)
+		
+		;
+
+
+	m.attr("WT_TOPLEVEL_SSD") = WT_TOPLEVEL_SSD;
+	m.attr("WT_TOPLEVEL_CSD") = WT_TOPLEVEL_CSD;
+	m.attr("WT_DESKTOP_COMPONENT") = WT_DESKTOP_COMPONENT;
+	m.attr("WT_POPOVER") = WT_POPOVER;
+
+	m.attr("WL_BACKGROUND") = WL_BACKGROUND;
+	m.attr("WL_BOTTOM") = WL_BOTTOM;
+	m.attr("WL_TOP") = WL_TOP;
+	m.attr("WL_OVERLAY") = WL_OVERLAY;
+	m.attr("WL_TOPLEAST") = WL_TOPLEAST;
+	m.attr("WL_TOPMOST") = WL_TOPMOST;
+
+	m.attr("WS_HIDDEN") = WS_HIDDEN;
+	m.attr("WS_NOT_HIDDEN") = WS_NOT_HIDDEN;
+	m.attr("WS_MINIMIZED") = WS_MINIMIZED;
+	m.attr("WS_NOT_MINIMIZED") = WS_NOT_MINIMIZED;
+	m.attr("WS_FULLSCREEN") = WS_FULLSCREEN;
+	m.attr("WS_NOT_FULLSCREEN") = WS_NOT_FULLSCREEN;
+	m.attr("WS_MAXIMIZED") = WS_MAXIMIZED;
+	m.attr("WS_NOT_MAXIMIZED") = WS_NOT_MAXIMIZED;
+
+	m.attr("WIM_ALWAYS") = WIM_ALWAYS;
+	m.attr("WIM_AUTO_WINDOW") = WIM_AUTO_WINDOW;
+	m.attr("WIM_NEVER") = WIM_NEVER;
+	
 } 

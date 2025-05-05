b0a83a46695a5f89f3b8b9fd18ba5f686e6f4acf
mi4code <114027049+mi4code@users.noreply.github.com>
2025-05-05 10:20:34 +0200

    rust: update library search path

--- rust/build.rs
+++ rust/build.rs
@@ -1,9 +1,9 @@
 fn main() {
 
     // set library (HUI build) directory
-    println!("cargo:rustc-link-search=native={}", "../build");
+    println!("cargo:rustc-link-search=native={}", "../../HUI/build");
 	
-	// deal with lib prefix weirdness
+    // deal with lib prefix weirdness
     if cfg!(target_os = "windows") {
         println!("cargo:rustc-link-lib=dylib=libHUI");
     } else {

1df3dbf847b94408772b7a4a4e453c43d5b97a1a
mi4code <114027049+mi4code@users.noreply.github.com>
2026-09-22 23:04:33 +0200

    Update hui_webview__blink_cef.cc

--- hui_webview__blink_cef.cc
+++ hui_webview__blink_cef.cc
@@ -1,5 +1,5 @@
-#ifndef _hui_backend_
-#define _hui_backend_
+#ifndef _hui_webview_
+#define _hui_webview_
 
 #include <ciso646>
 
@@ -469,8 +469,6 @@ void WebView::exit() {
 }
 
 
-#include "./hui_windowcontrols__dummy.cc"
-
 } // HUI
 
-#endif // _hui_backend_
+#endif // _hui_webview_
